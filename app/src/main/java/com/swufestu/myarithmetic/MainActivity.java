561ce7b5c71c3fda2426dfffce7693b0b7d95438
Ivy0819 <798603449@qq.com>
2021-10-02 18:30:07 +0800

    Initial commit

---
+++ app/src/main/java/com/swufestu/myarithmetic/MainActivity.java
@@ -0,0 +1,32 @@
+package com.swufestu.myarithmetic;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menu_set){
+            //openConfig();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
